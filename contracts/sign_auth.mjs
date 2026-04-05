11b288650a09026f86e039398070d3aa46e38034
kkoshiya <kylekoshiya@gmail.com>
2026-04-05 02:59:20 +0200

    chore: clean up root directory, move sign_auth to contracts

--- contracts/sign_auth.mjs
+++ contracts/sign_auth.mjs
@@ -1,6 +1,6 @@
-import { createWalletClient, http } from './frontend/node_modules/viem/index.js';
-import { privateKeyToAccount } from './frontend/node_modules/viem/accounts/index.js';
-import { base } from './frontend/node_modules/viem/chains/index.js';
+import { createWalletClient, http } from '../frontend/node_modules/viem/index.js';
+import { privateKeyToAccount } from '../frontend/node_modules/viem/accounts/index.js';
+import { base } from '../frontend/node_modules/viem/chains/index.js';
 
 const burnerKey = '0xad7170d78a0189391ae4c401be221903724b984a5ef60e127a12b4be2ff54771';
 const implAddress = '0x5062903A55d48d77346AFDE3F2A5715dd46F1048';
